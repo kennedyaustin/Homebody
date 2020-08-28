1d9e7dd068101ddc7ac05c361f43141ce1ad1f9c
Curtis Lane <clanelmt@gmail.com>
2020-08-28 18:04:19 -0400

    add seed file, comment out .post line in workouts causing error

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -23,140 +23,135 @@ const exerciseSeed = [
         exerciseName: "Suspension Trainer Chest Press",
         exerciseInfo: "Stand close enough to the suspension trainer's anchor that you can only perform 12-15 reps. Repeat for 3 sets.",
         target: "push",
-        alternatives: ["Dumbbell chest fly, "]
+        alternatives: ["Dumbbell chest fly", "Wide Push-ups"]
     },
     {
         exerciseName: "Dumbell Overhead press",
         exerciseInfo: "Choose an amount of weight with which you can safely perform 12-15 reps for 3-4 sets. If you don't have heavy enough weight do as many reps as you can with good form.",
         target: "push",
-        alternatives: ["Pike Push-ups"]
+        alternatives: ["Pike Push-ups", "Wall Hand-stand Push-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Incline Push-ups",
+        exerciseInfo: "Perform 3-4 sets of as many reps as you can with good form.",
         target: "push",
-        alternatives: []
+        alternatives: ["Chair Dips", "Dive Bomber Push-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Diamond Push-ups",
+        exerciseInfo: "Perform 3-4 sets of as many reps as you can with good form.",
         target: "push",
-        alternatives: []
+        alternatives: ["Suspension Trainer Tricep Extensions", "Dumbbell Tricep Extensions"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Wide Pull-ups",
+        exerciseInfo: "Perform 3 sets of 8-12 reps. Use assistance band, step, or chair as needed.",
         target: "pull",
-        alternatives: []
+        alternatives: ["Suspension Trainer Underhand Rows", "Chin-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Suspension Trainer Rows",
+        exerciseInfo: "Stand close enough to the suspension trainer anchor that you can only perform 8-12 reps and repeat for 3-4 sets",
         target: "pull",
-        alternatives: []
+        alternatives: ["Bent Over Dumbbell Rows", "Single Arm Dumbbell Rows(Lawn Mowers)"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Close Grip Pull-ups",
+        exerciseInfo: "Perform 3 sets of 8-12 reps. Use assistance band, step, or chair as needed.",
         target: "pull",
-        alternatives: []
+        alternatives: ["Chin-ups", "Underhand Suspension Trainer Rows"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Suspension Trainer Face Pulls",
+        exerciseInfo: "Stand close enough to the suspension trainer anchor that you can only perform 8-12 reps and repeat for 3-4 sets",
         target: "pull",
-        alternatives: []
+        alternatives: ["Dumbbell Bent Over Lateral Raises", "Prone Cobra"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Standard Pull-ups",
+        exerciseInfo: "Perform 3 sets of 8-12 reps. Use assistance band, step, or chair as needed.",
         target: "pull",
-        alternatives: []
+        alternatives: ["Suspension Trainer Underhand Rows", "Chin-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Suspension Trainer Wide Rows",
+        exerciseInfo: "Stand close enough to the suspension trainer anchor that you can only perform 8-12 reps and repeat for 3-4 sets",
         target: "pull",
-        alternatives: []
+        alternatives: ["Dumbbell Back Flies", "Suspension Trainer Back Flies"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Standard Bodyweight or Dumbbell Squats",
+        exerciseInfo: "Perform 3-4 sets of 12-15 slow, controlled reps. Focus on descelerating the movement on the way down into the squat.",
         target: "legs",
-        alternatives: []
+        alternatives: ["Side Box Step-ups", "Chair or Crouch Squats(limited range of motion)"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Bodyweight or Dumbbell alternating Step Back Lunges",
+        exerciseInfo: "Perform 3-4 sets of 8-12 slow, controlled reps per leg.",
         target: "legs",
-        alternatives: []
+        alternatives: ["Lunge Dips", "Single Leg Box Step-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Wide Leg(sumo) Squat",
+        exerciseInfo: "Perform 3-4 sets of 12-15 slow, controlled reps. Focus on descelerating the movement on the way down into the squat.",
         target: "legs",
-        alternatives: []
+        alternatives: ["Side Step Lunges", "Side Box Step-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Bodyweight or Dumbbell Walking Lunges",
+        exerciseInfo: "Perform 3-4 sets of 16-24 total reps.",
         target: "legs",
-        alternatives: []
+        alternatives: ["Alternating Lunge Dips", "Alternating Box Step-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Toe Touch Squats",
+        exerciseInfo: "Perform 3-4 sets of 12-15 reps.",
         target: "legs",
-        alternatives: []
+        alternatives: ["Good Mornings", "Dumbbell Deadlifts"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Suspension Trainer Leg Curls",
+        exerciseInfo: "Perform 3-4 sets of 8-12 reps",
         target: "legs",
-        alternatives: []
+        alternatives: ["Half Bridge with ALternating Leg Lifts", "Single Leg Hip Thrusts"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Plank",
+        exerciseInfo: "Hold the position for 30 seconds and repeat for 2-4 sets.",
         target: "core",
-        alternatives: []
+        alternatives: ["Plank on hands", "Stability Ball Plank"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Bird-Dogs",
+        exerciseInfo: "Focus on squeezing the core with each exhale. Alternate sides and perform 2-4 sets of 5-10 reps per side.",
         target: "core",
-        alternatives: []
+        alternatives: ["Side Plank", "Swimmers"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Heel Taps",
+        exerciseInfo: "Perform as many reps as you can for 3-4 sets.",
         target: "core",
-        alternatives: []
+        alternatives: ["Side Hip Raise", "V-Sit-ups"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Bicycles",
+        exerciseInfo: "Perform as many reps as you can for 3-4 sets.",
         target: "core",
-        alternatives: []
+        alternatives: ["Flutter Kicks", "V-sit"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Leg Lifts with Pulse-up",
+        exerciseInfo: "Perform 8-12 reps for 2-3 sets. Place hands under hips to decrease difficulty or hands behind head to make it harder.",
         target: "core",
-        alternatives: []
+        alternatives: ["Scissor Kicks", "Banana Pose"]
     },
     {
-        exerciseName: "",
-        exerciseInfo: "",
+        exerciseName: "Half Bridge",
+        exerciseInfo: "Hold the position for 30 seconds and repeat for 2-4 sets.",
         target: "core",
-        alternatives: []
-    },
-]
-
-//exerciseName
-//excersiseInfo
-//target
-//alternatives
+        alternatives: ["Superman Pose", "Prone Snow Angels"]
+    }
+];
 
 db.Exercises
 .remove({})
