5d10782364e976a41a16bed9d4ea8e5c1eef5340
Kyle Davis <kyled871@yahoo.com>
2020-09-02 19:32:11 -0400

    post request for logged user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
-// Get and Post for "/api/books"
+// Get and Post for "/api/users"
 router.route("/")
   .get(usersController.findAll)
   .post(usersController.login);
 
-// Get Put and Delete for "/api/books/:id"
+// Get Put and Delete for "/api/users/:id"
 router
   .route("/:id")
   .get(usersController.findById)
+  .post(usersController.postById)
   .put(usersController.update)
   .delete(usersController.remove);
 
