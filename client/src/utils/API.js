c258b8f8011d5985e286c4a58bc8762e4fb67003
Kyle Davis <kyled871@yahoo.com>
2020-09-09 20:17:09 -0400

    save workout info to profile/ workouts collection adds excercises

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 export default {
-  googleLogin: () => window.location.replace("/api/auth/google"),
-  facebookLogin: () => window.location.replace("/api/auth/facebook"),
+  // googleLogin: () => window.location.replace("/api/auth/google"),
+  // facebookLogin: () => window.location.replace("/api/auth/facebook"),
 
+  googleLogin: () => axios.post('/api/auth/google'),
+  facebookLogin: () => axios.post('/api/auth/facebook'),
+ 
   getUser: () => axios.post("/api/users/currentUser"),
   logoutUser: () => axios.post("/api/auth/logout"),
   // using post to retrieve all exercises from specified targets(array)
--- controllers/exercisesController.js
+++ controllers/exercisesController.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   update: function(req, res) {
     db
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .findOneAndUpdate({ _id: req.user._id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
