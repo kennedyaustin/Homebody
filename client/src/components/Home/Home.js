e2cbff6a81da63b9f02c8c6045be815f48ee43b0
Kyle Davis <kyled871@yahoo.com>
2020-09-12 15:06:23 -0400

    started animations/ drop down tabs

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,29 +2,37 @@ import React, { useContext } from "react";
 import BodyContext from "../../utils/BodyContext";
 import MuscleChoice from "../MuscleChoice/MuscleChoice";
 import SubmitButton from "../buttons/SubmitButton/SubmitButton";
+import Instructions from "../Instructions/Instructions";
 
 const Home = () => {
   const bodyContext = useContext(BodyContext);
 
   return (
-    <div className="container page">
-      <div className="row">
-        {bodyContext.targets.options.map((t) => (
-          <div key={t.name} className="col-6 col-md-3 mt-4">
-            <MuscleChoice
-              target={t.name}
-              value={t.name}
-              selected={t.selected}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="row mt-5">
-        <div className="col-8 offset-2 text-center">
-          <SubmitButton />
+    <>
+      <div className="container-fluid text-center">
+        <div className="row no-gutters d-flex justify-content-center">
+          <Instructions />
         </div>
       </div>
-    </div>
+      <div className="container page">
+        <div className="row">
+          {bodyContext.targets.options.map((t) => (
+            <div key={t.name} className="col-6 col-md-3 mt-4">
+              <MuscleChoice
+                target={t.name}
+                value={t.name}
+                selected={t.selected}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="row mt-5">
+          <div className="col-8 offset-2 text-center">
+            <SubmitButton />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- client/src/components/Instructions/Instructions.js
+++ client/src/components/Instructions/Instructions.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState} from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+import './style.css'
 
-function Instructions(){
+function Instructions() {
 
+    const [ isClicked, setIsClicked ] = useState(false)
+
+  return (
+    <div className="col-12 w-100 mx-auto">
+      <Accordion className="w-100" defaultActiveKey="0">
+        <Card className="bg-dark text-light">
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card className="bg-dark text-light">
+          <Card.Header>
+            <Accordion.Toggle as={Button} className="m-0 p-0 accordionButton" onClick={() => setIsClicked(!isClicked)} variant="link" eventKey="0">
+              {isClicked ? <img alt="" src="./down-arrow.png" /> : <img alt= "" src="./up-arrow.png" />}
+            </Accordion.Toggle>
+          </Card.Header>
+        </Card>
+      </Accordion>
+    </div>
+  );
 }
 
-export default Instructions
+export default Instructions;
